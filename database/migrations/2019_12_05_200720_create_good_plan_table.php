f68dca0e237e4ed02e8b3587536e8dc002eba7f4
Valentin Pollart <valentin@mathrix.fr>
2019-12-05 20:15:43 +0100

    feat: add first migrations

---
+++ database/migrations/2019_12_05_200720_create_good_plan_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodPlanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('good_plan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('link');
+            $table->unsignedBigInteger('institution_id');
+            $table->timestamp('ending_at');
+            $table->string('locations');
+            $table->string('category');
+            $table->json('filters');
+            $table->string('video_id');
+            $table->timestamps();
+
+            // Foreign keys
+            $table->foreign('institution_id')->references('id')->on('institutions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('good_plan');
+    }
+}
